e7e6968956dbb636d3a7d78b93c831e9795687d6
Saurabh Lodha <saurabhlodha2006@gmail.com>
2026-07-19 00:24:57 +0530

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.py
@@ -0,0 +1,17 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        times = []
+
+        combined = list(zip(position, speed))
+        combined.sort()
+        position[:], speed[:] = zip(*combined)
+
+        times.append((target-position[-1])/speed[-1])
+
+        for i in range(len(position)-2,-1,-1):
+            time = (target-position[i])/speed[i]
+
+            if time > times[-1]:
+                times.append(time)
+
+        return len(times)
